a8c7d4f60cc2f5b87e22a0e34ddbe27691d43fb0
Lina Ballesteros <lina.ballesteros13@gmail.com>
2023-08-09 18:06:51 -0500

    Formulario de búsqueda

--- movie/views.py
+++ movie/views.py
@@ -9,11 +9,7 @@ def home(request):
     # return HttpResponse('<h1>Welcome to Home Page <h1>')
     # return render(request, 'home.html', {'name': 'Lina Ballesteros'})
     searchTerm = request.GET.get('searchMovie')
-    if searchTerm:
-        movies = Movie.objects.filter(title__icontains=searchTerm)
-    else:
-        movies = Movie.objects.all()
-    return render(request, 'home.html', {'searchTerm':searchTerm, 'movies': movies})
+    return render(request, 'home.html', {'searchTerm':searchTerm})
 
 def about(request):
       return HttpResponse('<h1>Welcome to About Page</h1>')
